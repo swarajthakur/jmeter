960e2540628c1480fe27870803fa999e33e9755b
swarajthakur <swarajthakur@yahoo.com>
2019-04-01 18:36:12 +0100

    xzc

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 # Run JMeter Docker image with options
 
 NAME="jmeter"
-IMAGE="justb4/jmeter:3.3"
+IMAGE="justb4/jmeter:5.1.1"
 
 # Finally run
 docker stop ${NAME} > /dev/null 2>&1
 docker rm ${NAME} > /dev/null 2>&1
-docker run --name ${NAME} -i  $@
+docker run --name ${NAME} -i -v ${PWD}:${PWD} -w ${PWD} ${IMAGE} $@
